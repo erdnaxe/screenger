9a39902365cec61b04a08e95757ada3f73e7c2ba
Alexandre Iooss <erdnaxe@crans.org>
2020-04-30 13:43:54 +0200

    Page layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <section class="section is-medium has-text-centered">
-      <div class="container">
-        <img
-          alt="Screenger logo"
-          src="./assets/logo.svg"
-        >
-        <AudioUpload />
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered is-unselectable">
+          <img
+            alt="Screenger logo"
+            src="./assets/logo.svg"
+          >
+          <AudioUpload class="mt-2" />
+        </div>
+      </div>
+      <div class="hero-foot">
+        <AppFooter />
       </div>
     </section>
   </div>
@@ -14,6 +19,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import AppFooter from './components/AppFooter.vue'
 import AudioUpload from './components/AudioUpload.vue'
 import Buefy from 'buefy'
 import '@mdi/font/scss/materialdesignicons.scss'
@@ -23,9 +29,12 @@ Vue.use(Buefy)
 export default Vue.extend({
   name: 'App',
   components: {
+    AppFooter,
     AudioUpload
   }
 })
+
+document.title = 'Screenger - Make your screen sing'
 </script>
 
 <style lang="scss">
@@ -34,12 +43,46 @@ export default Vue.extend({
 
 // Custom colors
 $primary: #895ad6;
+$background: $grey-darker;
+$border: $grey-light;
+
 $body-background-color: darken($grey-darker, 4);
+$footer-background-color: $background;
+$button-background-color: $background;
+$button-border-color: lighten($button-background-color, 15);
+
+$title-color: #fff;
+$subtitle-color: $grey-light;
+$subtitle-strong-color: $grey-light;
+
 $text: #fff;
 $text-light: lighten($text, 10);
 $text-strong: darken($text, 5);
 
+$link: lighten($primary, 10);
+$link-hover: lighten($link, 5);
+$link-focus: darken($link, 5);
+$link-active: darken($link, 5);
+
+$button-color: $primary;
+$modal-card-body-background-color: darken($background, 5);
+$input-hover-color: $grey-light;
+$loading-background: $background;
+
+// Smaller footer
+$footer-padding: 1rem;
+
 // Import Bulma and Buefy styles
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
+
+// Hide scroll bar unless it is needed
+html {
+  overflow-y: auto;
+}
+
+// spacing
+.mt-2 {
+  margin-top: 2em;
+}
 </style>
--- src/components/AudioUpload.vue
+++ src/components/AudioUpload.vue
@@ -1,26 +1,30 @@
 <template>
-  <b-field>
-    <b-upload
-      v-model="dropFile"
-      drag-drop
-    >
-      <section class="section">
-        <div class="content has-text-centered">
-          <p>
-            <b-icon
-              icon="download"
-              size="is-large"
-            >
-            </b-icon>
-          </p>
-          <p>
-            Drop your songs here<br />
-            <small>or click to upload and start playing</small>
-          </p>
-        </div>
-      </section>
-    </b-upload>
-  </b-field>
+  <section>
+    <b-field>
+      <b-upload
+        v-model="dropFile"
+        drag-drop
+        :disabled="isLoading"
+        accept="audio/*"
+      >
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon
+                icon="download"
+                size="isLoading: false-large"
+              >
+              </b-icon>
+            </p>
+            <p>
+              Drop your songs here<br />
+              <small>or click to upload and start playing</small>
+            </p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+  </section>
 </template>
 
 <script lang="ts">
@@ -30,7 +34,26 @@ export default Vue.extend({
   name: 'AudioUpload',
   data () {
     return {
-      dropFile: null
+      dropFile: null,
+      isLoading: false
+    }
+  },
+  watch: {
+    // On upload, check file the prompt confirmation
+    dropFile: function (val) {
+      const size = (val.size / 1024 / 1024).toFixed(2) // in MiB
+      const message = `Continue on '${val.name}' (${size} MB, ${val.type})?<br/>`
+      this.$buefy.dialog.confirm({
+        message: message,
+        onConfirm: () => (this.isLoading = true)
+      })
+    },
+
+    // When loading start alert user
+    isLoading: function (val) {
+      if (val) {
+        this.$buefy.toast.open('Loading…')
+      }
     }
   }
 })
