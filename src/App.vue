5f519fc41fdbd49d70719b46380168c9b3d395cd
Alexandre Iooss <erdnaxe@crans.org>
2020-04-30 09:59:28 +0200

    Add Bulma style

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <section class="section is-medium has-text-centered">
+      <div class="container">
+        <img
+          alt="Vue logo"
+          src="./assets/logo.png"
+        >
+        <AudioUpload />
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import AudioUpload from './components/AudioUpload.vue'
+import Buefy from 'buefy'
+import '@mdi/font/scss/materialdesignicons.scss'
+
+Vue.use(Buefy)
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    AudioUpload
   }
 })
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Custom colors
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 </style>
---
+++ src/components/AudioUpload.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-field>
+    <b-upload
+      v-model="dropFile"
+      drag-drop
+    >
+      <section class="section">
+        <div class="content has-text-centered">
+          <p>
+            <b-icon
+              icon="upload"
+              size="is-large"
+            >
+            </b-icon>
+          </p>
+          <p>Drop your files here or click to upload</p>
+        </div>
+      </section>
+    </b-upload>
+  </b-field>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AudioUpload',
+  data () {
+    return {
+      dropFile: null
+    }
+  }
+})
+</script>
